60475cfbddb02e0b1d6c64db40e3292f8b7a7c6e
franasal <farcilas@gmail.com>
2022-05-19 12:00:36 +0200

    hot fix for st >1.8

--- streamlit_analytics/session_state.py
+++ streamlit_analytics/session_state.py
@@ -24,7 +24,7 @@ try:
     from streamlit.server.Server import Server
 except Exception:
     # Streamlit >= 0.65.0
-    import streamlit.script_run_context as ReportThread
+    import streamlit.scriptrunner as ReportThread
     from streamlit.server.server import Server
 
 
